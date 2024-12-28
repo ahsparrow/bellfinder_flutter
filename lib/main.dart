2803fef38b0bfc23ac2092a566637dda45268c70
Alan Sparrow <git@freeflight.org.uk>
2024-12-28 18:03:06 +0000

    Add sqflite_common_ffi for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
+import 'Dart:io' show Platform;
 import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'package:bellfinder/database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Add Linux support for testing
+  if (Platform.isLinux) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   final database = AppDatabase();
 
   await database.into(database.towers).insert(TowersCompanion.insert(
-        towerId: Value(1),
+        towerId: Value(123),
         place: 'Lockerley',
         dedication: 'S Johns',
         county: 'UK',
