398b1f97a372d72dbf1f02a31f7522078681c1d8
Alan Sparrow <git@freeflight.org.uk>
2025-01-04 18:40:37 +0000

    Add clustering

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,6 +1,7 @@
 import 'package:latlong2/latlong.dart' show LatLng;
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -42,8 +43,23 @@ class MapWidgetState extends State<MapWidget> {
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'uk.org.freeflight.bellfinder',
         ),
-        MarkerLayer(
-          markers: markers,
+        MarkerClusterLayerWidget(
+          options: MarkerClusterLayerOptions(
+            markers: markers,
+            builder: (context, markers) {
+              return Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue),
+                child: Center(
+                  child: Text(
+                    markers.length.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            },
+          ),
         ),
         RichAttributionWidget(
           attributions: [
