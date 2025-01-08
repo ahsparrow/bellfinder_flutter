7ced3e85e11d0f99d1c5a6600559000757466c08
Alan Sparrow <git@freeflight.org.uk>
2025-01-08 09:15:54 +0000

    Add floating action button to tower screen

--- lib/screens/tower_screen.dart
+++ lib/screens/tower_screen.dart
@@ -39,123 +39,131 @@ class TowerScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListenableBuilder(
-        listenable: viewModel,
-        builder: (context, _) {
-          final tower = viewModel.tower;
-
-          if (tower == null) {
-            // Empty widget
-            return SizedBox.shrink();
-          } else {
-            const spacer = TableRow(
-              children: [
-                SizedBox(height: 8),
-                SizedBox(height: 8),
-              ],
-            );
-
-            return DefaultTextStyle.merge(
-              style: Theme.of(context).textTheme.headlineSmall,
-              child: Container(
-                padding: EdgeInsets.all(8),
-                child: Table(
-                  columnWidths: const {
-                    0: IntrinsicColumnWidth(),
-                    1: FlexColumnWidth(),
-                  },
-                  children: [
-                    TableRow(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Text('Place:'),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(tower.place),
-                            Text(
-                              tower.dedication,
-                              textScaler: TextScaler.linear(0.75),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    spacer,
-                    TableRow(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Text('County:'),
-                        ),
-                        Text(tower.county),
-                      ],
-                    ),
-                    spacer,
-                    TableRow(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Text('Bells:'),
-                        ),
-                        Text(tower.bells.toString()),
-                      ],
-                    ),
-                    spacer,
-                    TableRow(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Text('Tenor:'),
-                        ),
-                        Text(viewModel.weightString),
-                      ],
-                    ),
-                    spacer,
-                    TableRow(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Text('Practice:'),
-                        ),
-                        Text(tower.practice),
-                      ],
-                    ),
-                    spacer,
-                    TableRow(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Text('First visit:'),
-                        ),
-                        // TBD - Add value
-                        Text(''),
-                      ],
-                    ),
-                    spacer,
-                    spacer,
-                    TableRow(
-                      children: [
-                        SizedBox.shrink(),
-                        Row(
-                          children: [
-                            FilledButton.tonal(
-                              onPressed: () => _launchUrl(tower.towerId),
-                              child: Text("Open Dove's Guide"),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }
-        },
+      body: _body(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return ListenableBuilder(
+      listenable: viewModel,
+      builder: (context, _) {
+        final tower = viewModel.tower;
+
+        if (tower == null) {
+          // Empty widget
+          return SizedBox.shrink();
+        } else {
+          const spacer = TableRow(
+            children: [
+              SizedBox(height: 8),
+              SizedBox(height: 8),
+            ],
+          );
+
+          return DefaultTextStyle.merge(
+            style: Theme.of(context).textTheme.headlineSmall,
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Table(
+                columnWidths: const {
+                  0: IntrinsicColumnWidth(),
+                  1: FlexColumnWidth(),
+                },
+                children: [
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Text('Place:'),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tower.place),
+                          Text(
+                            tower.dedication,
+                            textScaler: TextScaler.linear(0.75),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  spacer,
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Text('County:'),
+                      ),
+                      Text(tower.county),
+                    ],
+                  ),
+                  spacer,
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Text('Bells:'),
+                      ),
+                      Text(tower.bells.toString()),
+                    ],
+                  ),
+                  spacer,
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Text('Tenor:'),
+                      ),
+                      Text(viewModel.weightString),
+                    ],
+                  ),
+                  spacer,
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Text('Practice:'),
+                      ),
+                      Text(tower.practice),
+                    ],
+                  ),
+                  spacer,
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Text('First visit:'),
+                      ),
+                      // TBD - Add value
+                      Text(''),
+                    ],
+                  ),
+                  spacer,
+                  spacer,
+                  TableRow(
+                    children: [
+                      SizedBox.shrink(),
+                      Row(
+                        children: [
+                          FilledButton.tonal(
+                            onPressed: () => _launchUrl(tower.towerId),
+                            child: Text("Open Dove's Guide"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 
