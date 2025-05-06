9fefab55051669c85c27d2ed7fa2e7433e6d58c0
Alan Sparrow <git@freeflight.org.uk>
2025-05-06 17:13:00 +0100

    Redesign tower screen

--- lib/screens/tower_screen.dart
+++ lib/screens/tower_screen.dart
@@ -17,35 +17,10 @@ class TowerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BellFinder'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        actions: [
-          PopupMenuButton(
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                value: 1,
-                child: Text('Get directions'),
-              ),
-              PopupMenuItem(
-                value: 2,
-                child: Text('Add visit'),
-              ),
-            ],
-            onSelected: (val) async {
-              if (val == 1) {
-                final tower = viewModel.tower;
-                if (tower != null) {
-                  final uri = Uri.parse(
-                      "geo:${tower.latitude},${tower.longitude}?z=8&q=${tower.latitude},${tower.longitude}(${Uri.encodeFull(tower.dedication)})");
-                  await launchUrl(uri);
-                }
-              }
-            },
-          ),
-        ],
+        title: Text('Tower'),
       ),
       body: _body(context),
-      floatingActionButton: FloatingActionButton.extended(
+      floatingActionButton: FloatingActionButton(
         onPressed: () {
           final tower = viewModel.tower;
           if (tower != null) {
@@ -61,8 +36,7 @@ class TowerScreen extends StatelessWidget {
             );
           }
         },
-        icon: Icon(Icons.add),
-        label: Text("Add visit"),
+        child: Icon(Icons.add),
       ),
     );
   }
@@ -76,121 +50,121 @@ class TowerScreen extends StatelessWidget {
         if (tower == null) {
           // Empty widget
           return SizedBox.shrink();
-        } else {
-          const spacer = TableRow(
-            children: [
-              SizedBox(height: 8),
-              SizedBox(height: 8),
-            ],
-          );
+        }
 
-          return DefaultTextStyle.merge(
-            style: Theme.of(context).textTheme.headlineSmall,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  Table(
-                    columnWidths: const {
-                      0: IntrinsicColumnWidth(),
-                      1: FlexColumnWidth(),
-                    },
-                    children: [
-                      TableRow(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(right: 16),
-                            child: Text('Place:'),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(tower.place),
-                              Text(
-                                tower.dedication,
-                                textScaler: TextScaler.linear(0.75),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      spacer,
-                      TableRow(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(right: 16),
-                            child: Text('County:'),
-                          ),
-                          Text(tower.county),
-                        ],
-                      ),
-                      spacer,
-                      TableRow(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(right: 16),
-                            child: Text('Bells:'),
-                          ),
-                          Text(tower.bells.toString()),
-                        ],
-                      ),
-                      spacer,
-                      TableRow(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(right: 16),
-                            child: Text('Tenor:'),
-                          ),
-                          Text(viewModel.weightString),
-                        ],
-                      ),
-                      spacer,
-                      TableRow(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(right: 16),
-                            child: Text('Practice:'),
-                          ),
-                          Text(tower.practice),
-                        ],
-                      ),
-                      spacer,
-                      TableRow(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(right: 16),
-                            child: Text('First visit:'),
-                          ),
-                          Text((viewModel.firstVisit != null)
-                              ? DateFormat("d/M/y")
-                                  .format(viewModel.firstVisit!)
-                              : ""),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Center(
-                    child: FittedBox(
-                      child: Row(
-                        children: [
-                          FilledButton.tonal(
-                            onPressed: () => Navigator.pop(context, "map"),
-                            child: Text("Show Map"),
-                          ),
-                          SizedBox(width: 16),
-                          FilledButton.tonal(
-                            onPressed: () => _launchUrl(tower.towerId),
-                            child: Text("Open Dove's Guide"),
-                          ),
-                        ],
-                      ),
+        const spacer =
+            TableRow(children: [SizedBox(height: 4), SizedBox(height: 4)]);
+
+        return Column(
+          //Tower information header
+          children: [
+            Text(
+              tower.place,
+              style:
+                  DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2),
+            ),
+            Text(tower.dedication),
+
+            // Tower details table
+            Padding(
+              padding:
+                  EdgeInsets.only(right: 24, left: 24, top: 24, bottom: 48),
+              child: DefaultTextStyle.merge(
+                style: DefaultTextStyle.of(context)
+                    .style
+                    .apply(fontSizeFactor: 1.3),
+                child: Table(
+                  columnWidths: const {
+                    0: IntrinsicColumnWidth(),
+                    1: FlexColumnWidth(),
+                  },
+                  children: [
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 16),
+                          child: Text('County:'),
+                        ),
+                        Text(tower.county),
+                      ],
                     ),
-                  ),
-                ],
+                    spacer,
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 16),
+                          child: Text('Bells:'),
+                        ),
+                        Text(tower.bells.toString()),
+                      ],
+                    ),
+                    spacer,
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 16),
+                          child: Text('Tenor:'),
+                        ),
+                        Text(viewModel.weightString),
+                      ],
+                    ),
+                    spacer,
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 16),
+                          child: Text('Practice:'),
+                        ),
+                        Text(tower.practice),
+                      ],
+                    ),
+                    spacer,
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 16),
+                          child: Text('First visit:'),
+                        ),
+                        Text((viewModel.firstVisit != null)
+                            ? DateFormat("d/M/y").format(viewModel.firstVisit!)
+                            : ""),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          );
-        }
+            Center(
+              child: FittedBox(
+                child: Row(
+                  children: [
+                    OutlinedButton.icon(
+                      onPressed: () => Navigator.pop(context, "map"),
+                      label: Text("Map"),
+                      icon: Icon(Icons.map),
+                    ),
+                    SizedBox(width: 16),
+                    OutlinedButton.icon(
+                      onPressed: () => _launchUrl(tower.towerId),
+                      label: Text("Dove's"),
+                      icon: Icon(Icons.church_outlined),
+                    ),
+                    SizedBox(width: 16),
+                    OutlinedButton.icon(
+                      onPressed: () async {
+                        final uri = Uri.parse(
+                            "geo:${tower.latitude},${tower.longitude}?z=8&q=${tower.latitude},${tower.longitude}(${Uri.encodeFull(tower.dedication)})");
+                        await launchUrl(uri);
+                      },
+                      label: Text("Directions"),
+                      icon: Icon(Icons.directions_outlined),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
       },
     );
   }
