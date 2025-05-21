cad45f016ca8a04dbbf7e25452bac71a79622b8d
Alan Sparrow <git@freeflight.org.uk>
2025-05-21 20:43:57 +0100

    Show waiting for location message

--- lib/widgets/nearest_list.dart
+++ lib/widgets/nearest_list.dart
@@ -39,95 +39,107 @@ class NearestListWidgetState extends State<NearestListWidget> {
         listenable: widget.viewModel,
         builder: (context, _) {
           var nearest = widget.viewModel.getNearest();
-          return ListView.builder(
-            itemCount: nearest.length,
-            itemBuilder: (BuildContext context, int index) {
-              final nearby = nearest[index];
-              final tower = nearby.tower;
-              return GestureDetector(
-                onTap: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TowerScreen(
-                        viewModel: TowerViewModel(
-                          database: context.read<AppDatabase>(),
-                          towerId: tower.towerId,
+          if (nearest.isEmpty) {
+            return Center(
+              child: Text(
+                "Waiting for location...",
+                textAlign: TextAlign.center,
+                style: TextTheme.of(context).bodyLarge,
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: nearest.length,
+              itemBuilder: (BuildContext context, int index) {
+                final nearby = nearest[index];
+                final tower = nearby.tower;
+                return GestureDetector(
+                  onTap: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TowerScreen(
+                          viewModel: TowerViewModel(
+                            database: context.read<AppDatabase>(),
+                            towerId: tower.towerId,
+                          ),
                         ),
                       ),
-                    ),
-                  );
+                    );
 
-                  if (result == "map" && context.mounted) {
+                    if (result == "map" && context.mounted) {
+                      await widget.showTowerOnMap(context, tower);
+                    }
+                  },
+                  onLongPress: () async {
                     await widget.showTowerOnMap(context, tower);
-                  }
-                },
-                onLongPress: () async {
-                  await widget.showTowerOnMap(context, tower);
-                },
-                child: Card(
-                  margin: const EdgeInsets.all(2),
-                  child: ListTile(
-                    title: Row(
-                      crossAxisAlignment: CrossAxisAlignment.baseline,
-                      textBaseline: TextBaseline.alphabetic,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            tower.place,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Text("${(nearby.dist / 1609).toStringAsFixed(1)} mi"),
-                      ],
-                    ),
-                    subtitle: Row(
-                      crossAxisAlignment: CrossAxisAlignment.baseline,
-                      textBaseline: TextBaseline.alphabetic,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            '${tower.dedication}, ${tower.county}',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Text(
-                            "${HomeViewModel.weightCwt(tower.weight).round()} cwt"),
-                      ],
-                    ),
-                    leading: Stack(
-                      alignment: Alignment.topRight,
-                      children: [
-                        ...[
-                          CircleAvatar(
-                            backgroundColor: Color(
-                              bellColour(tower.bells, tower.unringable),
-                            ),
-                            child: Text('${tower.bells}'),
-                          )
-                        ],
-                        ...((widget.viewModel.hasVisit(tower.towerId))
-                            ? [
-                                Icon(
-                                  Icons.verified,
-                                  size: IconTheme.of(context).size! * 0.75,
-                                ),
-                              ]
-                            : []),
-                      ],
-                    ),
-                    leadingAndTrailingTextStyle:
-                        TextTheme.of(context).titleLarge,
-                    visualDensity: const VisualDensity(
-                        vertical: VisualDensity.minimumDensity),
-                  ),
-                ),
-              );
-            },
-          );
+                  },
+                  child: towerCard(tower, nearby.dist),
+                );
+              },
+            );
+          }
         },
+      ),
+    );
+  }
+
+  Card towerCard(Tower tower, double distance) {
+    return Card(
+      margin: const EdgeInsets.all(2),
+      child: ListTile(
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Expanded(
+              child: Text(
+                tower.place,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Text("${(distance / 1609).toStringAsFixed(1)} mi"),
+          ],
+        ),
+        subtitle: Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Expanded(
+              child: Text(
+                '${tower.dedication}, ${tower.county}',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Text("${HomeViewModel.weightCwt(tower.weight).round()} cwt"),
+          ],
+        ),
+        leading: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            ...[
+              CircleAvatar(
+                backgroundColor: Color(
+                  bellColour(tower.bells, tower.unringable),
+                ),
+                child: Text('${tower.bells}'),
+              )
+            ],
+            ...((widget.viewModel.hasVisit(tower.towerId))
+                ? [
+                    Icon(
+                      Icons.verified,
+                      size: IconTheme.of(context).size! * 0.75,
+                    ),
+                  ]
+                : []),
+          ],
+        ),
+        leadingAndTrailingTextStyle: TextTheme.of(context).titleLarge,
+        visualDensity:
+            const VisualDensity(vertical: VisualDensity.minimumDensity),
       ),
     );
   }
