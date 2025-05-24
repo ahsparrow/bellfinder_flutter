30477469117d6f1be662c86eb5665116f93e4f09
Alan Sparrow <git@freeflight.org.uk>
2025-05-24 13:24:11 +0100

    Make editvisit screen scrollable

--- lib/screens/editvisit_screen.dart
+++ lib/screens/editvisit_screen.dart
@@ -70,114 +70,118 @@ class EditFormState extends State<EditForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ListTile(
-          title: Text(
-            widget.viewModel.place,
-            style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2),
-          ),
-          subtitle: Text(widget.viewModel.dedication),
-          leading: CircleAvatar(
-            radius: 30,
-            backgroundColor: Color(bellColour(widget.viewModel.bells)),
-            child: Text(
-              "${widget.viewModel.bells}",
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: Text(
+              widget.viewModel.place,
               style:
                   DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2),
             ),
-          ),
-        ),
-
-        // Date
-        Padding(
-          padding: const EdgeInsets.fromLTRB(8, 16, 8, 8),
-          child: TextField(
-            readOnly: true,
-            controller: _dateController,
-            onTap: () => _pickDate(context),
-            decoration: const InputDecoration(
-              labelText: "Date",
-              border: OutlineInputBorder(),
+            subtitle: Text(widget.viewModel.dedication),
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundColor: Color(bellColour(widget.viewModel.bells)),
+              child: Text(
+                "${widget.viewModel.bells}",
+                style:
+                    DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2),
+              ),
             ),
           ),
-        ),
 
-        // Notes
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: TextField(
-            minLines: 5,
-            maxLines: 5,
-            controller: _noteController,
-            decoration: const InputDecoration(
-              labelText: "Notes",
-              border: OutlineInputBorder(),
+          // Date
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 16, 8, 8),
+            child: TextField(
+              readOnly: true,
+              controller: _dateController,
+              onTap: () => _pickDate(context),
+              decoration: const InputDecoration(
+                labelText: "Date",
+                border: OutlineInputBorder(),
+              ),
             ),
           ),
-        ),
 
-        // Quarter
-        CheckboxListTile(
-          title: const Text("Quarter"),
-          onChanged: (value) {
-            setState(() {
-              _quarter = value ?? false;
-            });
-          },
-          value: _quarter,
-          controlAffinity: ListTileControlAffinity.leading,
-        ),
+          // Notes
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: TextField(
+              minLines: 5,
+              maxLines: 5,
+              controller: _noteController,
+              decoration: const InputDecoration(
+                labelText: "Notes",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
 
-        // Peal
-        CheckboxListTile(
-          title: const Text("Peal"),
-          onChanged: (value) {
-            setState(() {
-              _peal = value ?? false;
-            });
-          },
-          value: _peal,
-          controlAffinity: ListTileControlAffinity.leading,
-        ),
+          // Quarter
+          CheckboxListTile(
+            title: const Text("Quarter"),
+            onChanged: (value) {
+              setState(() {
+                _quarter = value ?? false;
+              });
+            },
+            value: _quarter,
+            controlAffinity: ListTileControlAffinity.leading,
+          ),
 
-        Row(
-          children: [
-            const Spacer(),
+          // Peal
+          CheckboxListTile(
+            title: const Text("Peal"),
+            onChanged: (value) {
+              setState(() {
+                _peal = value ?? false;
+              });
+            },
+            value: _peal,
+            controlAffinity: ListTileControlAffinity.leading,
+          ),
 
-            // Delete button
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () async {
-                  if (await _confirmDelete() && context.mounted) {
+          Row(
+            children: [
+              const Spacer(),
+
+              // Delete button
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (await _confirmDelete() && context.mounted) {
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text("Delete"),
+                ),
+              ),
+
+              // Save button
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: FilledButton(
+                  onPressed: () {
+                    widget.viewModel.update(
+                      date:
+                          DateFormat('dd/MM/yyyy').parse(_dateController.text),
+                      notes: _noteController.text,
+                      peal: _peal,
+                      quarter: _quarter,
+                    );
                     Navigator.pop(context);
-                  }
-                },
-                child: Text("Delete"),
+                  },
+                  child: const Text("Save"),
+                ),
               ),
-            ),
-
-            // Save button
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: FilledButton(
-                onPressed: () {
-                  widget.viewModel.update(
-                    date: DateFormat('dd/MM/yyyy').parse(_dateController.text),
-                    notes: _noteController.text,
-                    peal: _peal,
-                    quarter: _quarter,
-                  );
-                  Navigator.pop(context);
-                },
-                child: const Text("Save"),
-              ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 
