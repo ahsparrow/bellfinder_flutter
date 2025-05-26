ac31a92bb92d90134104cff20c4be82ae4851259
Alan Sparrow <git@freeflight.org.uk>
2025-05-26 11:14:31 +0100

    Remove release ndk debugSymbolLevel setting

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,9 +50,6 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            ndk {
-              debugSymbolLevel = "FULL"
-            }
         }
     }
 }
